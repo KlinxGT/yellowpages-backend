9692f509a8ef5ec72db3568274d5e174ece8887b
Xin Lin <xl26@email.sc.edu>
2024-06-12 04:47:17 -0400

    yep

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const app = express();
 const Joi = require("joi");
 const multer = require("multer");
+const mongoose = require("mongoose");
 
 app.use(express.static("public"));
 app.use(cors());
@@ -18,72 +19,32 @@ const storage = multer.diskStorage({
 
 const upload = multer({storage:storage});
 
-let posts = [
-    {
-        "_id":"1",
-        "image":"images/abook.jpg",
-        "caption":"Just read 1984 by George Orwell. God what a read; it quite literally blew my mind. Just the sheer depth of the story and the message behind it. I'm sure most people wouldn't understanding the subtleties to it but it really struck me to the core. Blah blah blah I'm so smart, look at me yall I read a book that's deep, I'm deep and wise and better than everyone. Please please please give me the validation and approval I never got from the friends I never had.",
-        "likeCount":"-400"
-    },
-    {
-        "_id":"2",
-        "image":"images/angel.jpg",
-        "caption":"Stumbled across a mural, beautiful. A mind in pain is just a soul longing to return to heaven. *insert pinterest quote and hashtags*",
-        "likeCount":"5"
-    },
-    {
-        "_id":"3",
-        "image":"images/coffee.jpg",
-        "caption":"Cute cafe I went to that I'll deliberately not say the name of and when asked, will gatekeep and reply: Honestly it's a really low key place and a sanctuary to me. Also the coffee in question is 16 dollar plus the 40% tip that was somehow the lowest option on the ipad they spun around for you to choose your tip.",
-        "likeCount":"20"
-    },
-    {
-        "_id":"4",
-        "image":"images/trustfund.jpg",
-        "caption":"Our parents work for lockheed martin and we have trust funds.",
-        "likeCount":"70"
-    },
-    {
-        "_id":"5",
-        "image":"images/dreams.jpg",
-        "caption":"I bought a rock off of ebay for 400$ and now I claim I'm in touch with the spirits in nature and can cure liver cancer through sound waves and vibrations. Buy my 50$ essential oil kit and I'll throw in a juice cleanse that's basically laxatives",
-        "likeCount":"4"
-    },
-    {
-        "_id":"6",
-        "image":"images/couple.jpg",
-        "caption":"Something something love something something he's my king she's my queen.",
-        "likeCount":"07149874"
-    },
-    {
-        "_id":"7",
-        "image":"images/manbun.jpg",
-        "caption":"Manbun, enough said",
-        "likeCount":"-00.00"
-    },
-    {
-        "_id":"8",
-        "image":"images/coffee.jpg",
-        "caption":"I don't even like coffee, more of a tea guy tbh",
-        "likeCount":"40"
-    }, 
-    {
-        "_id":"9",
-        "image":"images/stones.jpg",
-        "caption":"Pretty neat since it takes effor, but also why bother taking a pic of it aren't you supposed to be zen and self reflective, the opposite of social media?",
-        "likeCount":"4"
-    }
-];
+mongoose
+  .connect("mongodb+srv://xl26:wvVE5g5FRwv7HSNX@mongobongo.r0e3dod.mongodb.net/?retryWrites=true&w=majority&appName=MongoBongo")
+  .then(() => console.log("Connected to mongodb..."))
+  .catch((err) => console.error("could not connect ot mongodb...", err));
+
+
+const postSchema = new mongoose.Schema({
+    image: String,
+    caption: String,
+    likeCount: Number,
+    extraInfo: String, 
+    tags: String
+});
+
+const Posts = mongoose.model("Post", postSchema);
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/api/posts", (req, res) => {
+app.get("/api/posts", async (req, res) => {
+    const posts = await Posts.find();
     res.send(posts);
 });
 
-app.post("/api/posts", upload.single("img"), (req, res) => {
+app.post("/api/posts", upload.single("img"), async(req, res) => {
 
     const result = validatePost(req.body);
     
@@ -92,29 +53,63 @@ app.post("/api/posts", upload.single("img"), (req, res) => {
         return;
     }
 
-    const post = {
-        _id: posts.length+1,
+    const post = new Posts({
         image: req.file!= null? "images/" + req.file.filename : "" ,
         caption: req.body.caption,
-        likeCount: 0    
-    };
+        likeCount: 0,
+        extraInfo:req.body.extraInfo,
+        tags:req.body.tags  
+    });
     /*
     if(req.file) {
         post.image = "images/" +req.file.filename;
     }
     */
-    posts.push(post);
-    res.status(200).send(post);
+    
+    const newPost = await post.save();
+    res.send(newPost);
 
     console.log("valid");
 });
 
+app.put("/api/posts/:id", upload.single("img"), async(req, res) => {
+    const result = validatePost(req.body);
+   
+    if (result.error) {
+      res.status(400).send(result.error.details[0].message);
+      return;
+    }
+
+    let fieldsToUpdate = {
+        caption: req.body.caption,
+        extraInfo: req.body.extraInfo,
+        tags: req.body.tags,
+        image: req.file!=null?"images/" + req.file.filename : ""
+    }
+
+    
+    console.log("made it");
+  
+   const wentThrough = await Posts.updateOne(
+    {_id: req.params.id},
+    fieldsToUpdate
+   );
+
+   const updatedHouse = await Posts.findOne({_id: req.params.id});
+   res.send(updatedHouse);
+  });
+
+app.delete("/api/posts/:id", async(req, res) => {
+    const post = await Posts.findByIdAndDelete(req.params.id);
+    res.send(post);   
+    console.log("delete");
+});
 
 const validatePost = (post) => {
     const schema = Joi.object({
         _id: Joi.allow(""),
         caption: Joi.string().required(),
-        description: Joi.string().required(),
+        extraInfo: Joi.string().required(),
         tags: Joi.string().required()
     });
 
